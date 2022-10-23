e9cfa5df8acb350ee618be505267393745f9f1dd
D1g1talEntr0py <jason.dimeo@gmail.com>
2022-10-23 17:16:20 -0400

    Fixed bug with _arrayRemove() where 'this' was being passed to Array.prototype.push.call() as the first parameter with a reference to 'undefined'.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # chrysalis
 JavaScript Utility Modules and Extensions
+
+This is a collection of various utility functions.
+You can either import an ES module or add to the prototype of an existing object.
+
+### Installation:
+```bash
+npm install @d1g1tal/chrysalis --save
+```
+
+### Usage:
+```javascript
+import { _arrayIsEmpty } from '@d1g1tal/collections';
+
+const array = [];
+
+console.log(`Array is empty? ${_arrayIsEmpty(array)}`); // Array is empty? true
+```
--- src/esm/array-remove.js
+++ src/esm/array-remove.js
@@ -4,14 +4,14 @@
  *
  * @param {Array} array The array to remove from
  * @param {number} from Starting position
- * @param {number} [to] Ending position
+ * @param {number?} to Ending position
  * @returns {number} The new length of the array
  */
 const _arrayRemove = (array, from, to) => {
 	const rest = array.slice((to || from) + 1 || array.length);
 	array.length = from < 0 ? array.length + from : from;
-	
-	return array.push.apply(this, rest);
+
+	return array.push(...rest);
 };
 
 export default _arrayRemove;
