253b467a4dc4357bdfac8403aac2702265042f09
D1g1talEntr0py <jason.dimeo@gmail.com>
2023-02-01 15:55:01 -0500

    Minor refactor to _objectMerge and _arrayRemove -Updated dependencies -Refactored _objectMerge loop to use for ... of instead of .forEach -Refactored _arrayRemove to set default value for 'to' parameter -Added unit test for _objectMerge

--- src/esm/array-remove.js
+++ src/esm/array-remove.js
@@ -4,11 +4,11 @@
  *
  * @param {Array} array The array to remove from
  * @param {number} from Starting position
- * @param {number?} to Ending position
+ * @param {number} [to=from] Ending position
  * @returns {number} The new length of the array
  */
-const _arrayRemove = (array, from, to) => {
-	const rest = array.slice((to || from) + 1 || array.length);
+const _arrayRemove = (array, from, to = from) => {
+	const rest = array.slice(to + 1 || array.length);
 	array.length = from < 0 ? array.length + from : from;
 
 	return array.push(...rest);
--- tests/object-merge.test.js
+++ tests/object-merge.test.js
@@ -2,8 +2,10 @@ import { describe, expect, test } from '@jest/globals';
 import { _objectMerge } from '../modules.js';
 
 describe('_objectMerge', () => {
-	const obj = { headers: { Accept: 'application/json' } };
-	const defaults = { headers: {} };
+	const config = { headers: { Accept: ['application/json'] } };
+	const defaults = { path: '/api/music/artists', headers: {} };
 
-	test('Merge Nested Objects', () => expect(_objectMerge(obj, defaults)).toEqual({ headers: { Accept: 'application/json' } }));
+	test('Merge 2 objects', () => expect(_objectMerge(config, defaults)).toEqual({ headers: { Accept: ['application/json'] } }));
+	test('Merge 3 objects', () => expect(_objectMerge({ path: '/api/music/artists' }, defaults, config)).toEqual({ path: '/api/music/artists', headers: { Accept: ['application/json'] } }));
+	test('Merge objects containing arrays', () => expect(_objectMerge({ path: '/api/music/artists', headers: { Accept: ['text/xml'] } }, defaults, config)).toEqual({ path: '/api/music/artists', headers: { Accept: ['application/json', 'text/xml'] } }));
 });
