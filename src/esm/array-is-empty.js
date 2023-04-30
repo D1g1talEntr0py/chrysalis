5b6184db093984d888a24ab431c3192ceb2d24ad
D1g1talEntr0py <jason.dimeo@gmail.com>
2023-04-30 18:32:28 -0400

    Bug fix for _arrayIsEmpty()

--- src/esm/array-is-empty.js
+++ src/esm/array-is-empty.js
@@ -1,9 +1,9 @@
 /**
  * Checks to see if the array is empty
  *
- * @param {Array} array The array to check.
+ * @param {Array<*>} array The array to check.
  * @returns {boolean} true if the array is empty, false otherwise.
  */
-const _arrayIsEmpty = (array) => array?.length > 0;
+const _arrayIsEmpty = (array) => array?.length == 0;
 
 export default _arrayIsEmpty;
